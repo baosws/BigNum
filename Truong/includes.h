206c45c3ae14bca212a588274b75fbc4bfbfb381
HXTruong <hoangxuantruong98@gmail.com>
2018-03-13 09:39:18 +0700

    Add function add/subtract and BigNum

---
+++ Truong/includes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include "BigNum.h"
+//#include "BigDec.h"
+//#include "BigInt.h"
+//#include "BigFloat.h"
+
+#define LENGTH_OF_BITS 128
+
+using namespace std;
