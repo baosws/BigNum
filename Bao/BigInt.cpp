825a08e4662f56e13782e39cb1df56906419f6b8
bao <baosws@gmail.com>
2018-03-15 21:42:30 +0700

    added Bao directory

---
+++ Bao/BigInt.cpp
@@ -0,0 +1,21 @@
+#include "../includes.h"
+
+BigInt BigInt::operator~() const {
+	return BigInt(!(data[0] || data[1]));
+}
+
+BigInt BigInt::operator~() const {
+	return BigInt({~data[0], ~data[1]});
+}
+
+BigInt BigInt::operator&(const BigInt& other) const {
+	return BigInt({this->data[0] & other.data[0], this->data[1] & other.data[1]});
+}
+
+BigInt BigInt::operator|(const BigInt& other) const {
+	return BigInt({this->data[0] | other.data[0], this->data[1] | other.data[1]});
+}
+
+BigInt BigInt::operator^(const BigInt& other) const {
+	return BigInt({this->data[0] ^ other.data[0], this->data[1] ^ other.data[1]});
+}
