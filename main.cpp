ff607ce2bc3fb1526de4c14ece3a3c9a92d6ee8e
bao <baosws@gmail.com>
2018-03-21 01:32:19 +0700

    casting operators

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include "helper.h"
 
 int main() {
-	BigInt a = 5;
-	BigInt b = 4;
-	tell(a);
-	tell(b);
-	tell(b-a);
-	tell(0-b);
-	tell(a+b);
+// 	BigInt a = 5;
+// 	BigInt b = 4;
+// 	tell(a);
+// 	tell(b);
+// 	tell(b-a);
+// 	tell(0-b);
+// 	tell(a+b);
+// 	tell(a<<10);
+// 	tell(a>>1);
+	BigInt a(10);
+	cout << a << endl;
+	for (int i = 0; i < 128; i++)
+		cout << a.get_bit(i);
 }
