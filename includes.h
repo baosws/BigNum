267de63d0e4dbed3cdc57d0da53a00fa7b0125c2
chienfx <chienpham304@gmail.com>
2018-03-24 11:45:31 +0700

    added some method to BigFloat class

--- includes.h
+++ includes.h
@@ -6,7 +6,7 @@ using namespace std;
 #include "BigNum.h"
 //#include "BigDec.h"
 #include "BigInt.h"
-//#include "BigFloat.h"
+#include "BigFloat.h"
 
 const int __LENGTH_OF_BITS = 128;
 const int MAX_DEC_LENGTH = 39;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "includes.h"
-#include "helper.h"
+//#include "helper.h"
 
 void test() {
 	freopen("testcases/test.in", "r", stdin);
@@ -20,7 +20,7 @@ void test() {
 }
 
 int main() {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //<<<<<<< HEAD
 // 	BigInt a = 5;
 // 	BigInt b = 4;
@@ -52,12 +52,12 @@ int main() {
 	tell(a+b);*/
 //>>>>>>> ff8004af70c52afe12a4f81287a559e38440b394
 //>>>>>>> ee3fd9b8ad070e117796d788b4c63631f16c3152
-=======
+//=======
 // 	BigInt a = 7;
 // 	BigInt b = -5;
 // 	cout << a << endl;
 // 	cout << b << endl;
 // 	cout << a / b << endl;
 	test();
->>>>>>> bd12e7a44220dc13fb0c52eabdbb0275adbba7c0
+//>>>>>>> bd12e7a44220dc13fb0c52eabdbb0275adbba7c0
 }
