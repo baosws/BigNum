d137d00d60b16036bd14f2b6a58c1bf09a3fc176
chienfx <chienpham304@gmail.com>
2018-03-30 21:54:42 +0700

    diana sieu tham

--- main.cpp
+++ main.cpp
@@ -113,7 +113,7 @@ void process(const vector<string>& args) {
 }
 int main(int nargs, char* args[]) {
 	freopen(args[1], "r", stdin);
-	freopen(args[2], "w", stdout);
+//	freopen(args[2], "w", stdout);
 	string line;
 	for (int i = 0; std::getline(cin, line); ++i) {
 		stringstream parser(line);
@@ -123,5 +123,6 @@ int main(int nargs, char* args[]) {
 			args.push_back(arg);
 		}
 		process(args);
+		cout <<  endl;
 	}
 }
